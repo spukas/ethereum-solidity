c59661a4905889a136368ebc9763173f4e46b51e
Linas Spukas <l.spukas@gmail.com>
2018-02-15 19:17:23 +0200

    Add test to check for contract and initial message

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
 
 // set local instance of test network
 // provider() is the connecting link between web3 and ganache
-const web3 = new Web3(ganache.provider());
-
-const { interface, bytecode } = require('../compile');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 let accounts;
 let inbox;
@@ -18,10 +18,17 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
+
+  inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
   });
 });
